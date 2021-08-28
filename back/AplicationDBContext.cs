dd1a94e4a04a5afbaa39f512281fb3984d8a3a3f
Antonio Andrade <antoniomaresmares001gmail.com>
2021-08-28 13:45:13 -0500

    Primer version

---
+++ back/AplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    //crea una instancia a nuestra base de datos para poder hacer Consultas y demas operaciones
+    public class AplicationDBContext:DbContext
+    {
+        //1dbste, 2modelo, 3nombreBaseDatos 
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; } //Mapeando modelos con la tabla de la DB
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options):base (options)
+        {
+
+        }
+
+    }
+}
---
+++ front/src/app/services/tarjeta.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarjetaService {
+
+  private myAppUrl = 'https://localhost:44352/';
+  private myApiUrl = 'api/tarjeta/'
+
+  constructor(private http: HttpClient) {}
+
+  //Observable se usa para hacer peticiones asincronas
+  
+  getListTarjetas():Observable<any>{
+    return this.http.get(this.myAppUrl + this.myApiUrl)
+  }
+
+  deleteTarjeta(id:number):Observable<any>{
+    return this.http.delete( this.myAppUrl + this.myApiUrl + id);
+  }
+
+  saveTarjeta(tarjeta:any):Observable<any>{
+    return this.http.post( this.myAppUrl + this.myApiUrl, tarjeta)
+
+  }
+
+  updateTarjeta(id:number, tarjeta:any):Observable<any>{
+    return this.http.put( this.myAppUrl + this.myApiUrl + id, tarjeta)
+
+  }
+}
